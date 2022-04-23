ae689b6420df050e845348ecc46c0a06d9f84014
AnnaLuniushkina <anna.luniushkina@gmail.com>
2022-04-23 20:32:14 +0300

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+const disabled = true;
+let timeInterval;
+
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+
+function changeBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartClick() {
+    btnStart.disabled = disabled;
+    btnStop.disabled = !disabled;
+    timeInterval = setInterval(changeBgColor, 1000);
+}
+
+function onStopClick() {
+    btnStart.disabled = !disabled;
+    btnStop.disabled = disabled;
+    clearInterval(timeInterval);
+}
